c9711b0198dcb8f2b51db892153f0b0b5b4437af
lucastala <lucastalavera25@gmail.com>
2022-06-16 21:13:52 -0300

    proyecto final primer entrega

--- js/archivo.js
+++ js/archivo.js
@@ -54,30 +54,54 @@
 // alert("Felicitaciones, se aplico el descuento. Precio abonar: "+ preciofinal);  
 
 function ingresarproducto() {
-    let nombredelproducto=prompt("ingrese nombre del producto") 
-    let preciodelproducto=prompt("ingrese el precio del producto") 
-    let producto= {
-        nombre:nombredelproducto, 
-        precio: Number(preciodelproducto) 
-    }   
+    let producto={}
+    producto.nombre = prompt("ingrese nombre del producto") 
+    producto.precio = Number(prompt("ingrese el precio del producto")) 
+
     return producto
+}  
+function carritoToString(carrito) {
+    let stringproducto=""
+    for (let i = 0; i < carrito.length; i++) {
+        stringproducto= stringproducto + carrito[i].nombre+" $"+carrito[i].precio + "\r"
+    } 
+    return stringproducto
 }
+let nombreyApellido=prompt("ingrese su nombre y apellido") 
+alert("bienvenido al marketplace " + nombreyApellido); 
 let ingreso
 let carrito=[] 
 let preciodelcarrito=0
-do  { 
-    ingreso=Number(prompt())
+do  {
+
+    ingreso=Number(prompt(
+        nombreyApellido+ `\r
+Para ingrersar al sistema ingrese 1\r
+Para ver el monto a abonar ingrese 2\r
+Si desea salir ingrese 0\r\r
+Carrito:
+`+ carritoToString(carrito)
+    ))
     switch (ingreso) {
         case 1: 
-        let producto=ingresarproducto() 
-        carrito.push(producto) 
-        preciodelcarrito= preciodelcarrito + producto.precio
-        break;
+            let producto=ingresarproducto() 
+            carrito.push(producto) 
+            preciodelcarrito= preciodelcarrito + producto.precio
+
+            break;
+        case 2: 
+            alert(nombreyApellido+ " debe abonar " +preciodelcarrito)
+            break;
+
+        case 0:
+            alert(nombreyApellido + " gracias por tu visita!") 
+            break;
         
         default:
-            break; 
-            
-        } 
+            alert(nombreyApellido + ", esa opción no esta en el menu")
+            break;      
+        }
+         
     }    while (ingreso !=0) 
     console.log(carrito); 
     console.log(preciodelcarrito);
